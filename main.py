a4a0f523b62e3651af1aeb4471bd70eade448d4c
delaneymizell <83244330+delaneymizell@users.noreply.github.com>
2021-09-07 15:44:43 -0400

    moved folder, added dict for user and pass

--- main.py
+++
@@ -1,7 +0,0 @@
-def main ():
-    print("this is delaneys work")
-
-def getname ():
-    username = input()
-
-
---
+++ mainsection/main.py
@@ -0,0 +1,8 @@
+def main ():
+    print("this is delaneys work")
+
+def getname ():
+    uandp= [('user', input()), ('passw', input())]
+    usernum1 = dict(uandp)
+    print("the username is " + usernum1['user'] + " and the password is " + usernum1['passw'] + "!")
+
