e1164084c69c525ac7d1fc0393bcda2105a06f98
delaneymizell <83244330+delaneymizell@users.noreply.github.com>
2021-09-08 22:03:40 -0400

    this added some simple loop lists

--- mainsection/main.py
+++ mainsection/main.py
@@ -1,8 +1,14 @@
 def main ():
     print("this is delaneys work")
 
-def getname ():
+def dictprac ():
     uandp= [('user', input()), ('passw', input())]
     usernum1 = dict(uandp)
     print("the username is " + usernum1['user'] + " and the password is " + usernum1['passw'] + "!")
+
+def loopprac ():                            #simple loop prac
+    for x in range(1,10):                   #easist way to make a certain length loop
+        print("the value of x is %d" %x)    #%d is integer %s is string
+    else:
+        print("the value is not in range")  #simple else 
 
