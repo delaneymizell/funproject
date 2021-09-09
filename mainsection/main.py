bf873e0cdfc7ff4883b0a7ebcfbf822d3f3c3bda
delaneymizell <83244330+delaneymizell@users.noreply.github.com>
2021-09-09 17:39:06 -0400

    added some additions to loop and metho tests

--- mainsection/main.py
+++ mainsection/main.py
@@ -6,9 +6,25 @@ def dictprac ():
     usernum1 = dict(uandp)
     print("the username is " + usernum1['user'] + " and the password is " + usernum1['passw'] + "!")
 
-def loopprac ():                            #simple loop prac
+def loopprac (word):                            #simple loop prac
     for x in range(1,10):                   #easist way to make a certain length loop
         print("the value of x is %d" %x)    #%d is integer %s is string
     else:
         print("the value is not in range")  #simple else 
+    personname = ['bob', 'sally', 'john']   #list of names
+    personage = [12, 25, 46]                #list of ages
+    for x in range(len(personname)):        #loop length based on how many names
+        print('person name and age', personname[x], personage[x], enumerate[x])
+
+    word = 'dummy'
+    while word:
+        word = input()
+        print('you entered this' , word)
+        if word == 'stop':
+            break
+
+def caller(loopprac):
+
+    
+
 
