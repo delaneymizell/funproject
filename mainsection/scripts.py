56922064aeb9c07636a022e7329bf0333f9da8d4
delaneymizell <83244330+delaneymizell@users.noreply.github.com>
2021-09-12 21:08:19 -0400

    daily

--- mainsection/scripts.py
+++ mainsection/scripts.py
@@ -2,3 +2,5 @@ import subprocess
 
 subprocess.check_output(["echo", "Hello World"])
 # this is disgusting and needs alot of work :barf:
+
+#dailey commit to stay active, lazy delaney you dhould be punished for this
