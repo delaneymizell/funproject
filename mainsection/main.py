9d1dc05c64407efeed75243ef5b66c71e8d96dc3
delaneymizell <83244330+delaneymizell@users.noreply.github.com>
2022-02-06 19:38:18 -0500

    big commit

--- mainsection/main.py
+++ mainsection/main.py
@@ -1,4 +1,5 @@
 import os
+import datetime as datetime
 
 def main ():
     print("this is delaneys work")
@@ -33,9 +34,70 @@ def lastindexof():
     filetype = string1[indexofperiod:len(string1)]
     print(filetype)
 
+def testy():
+    name = "john"
+    name2 = "john is bad"
+    print(name2.isalpha())
+
+def timetest():
+    import datetime as datetime
+    import time as time
+    oldtime = datetime.datetime.now()
+    time.sleep(3)
+    nowtime = datetime.datetime.now()
+    print(oldtime)
+    print(nowtime)
+    difftime = nowtime - oldtime
+    if oldtime < nowtime:
+        print("old time time bigger by more than 2 seconds")
+        print(difftime)
+    #elif nowtime > currenttime: 
+     #   print("now bigger than current")
+    else:
+        print("something broke")
+
+    print(nowtime)
+
+def timetest2():
+    import datetime as datetime
+    now = "12/15/21 14:00"
+    truedate = datetime.datetime.strptime(now, '%m/%d/%y %H:%M')
+    timenow = datetime.datetime.now()
+    print(now)
+    print(truedate)
+    if truedate < timenow:
+        print("notbased")
+    else:
+        print("based")
+
+def trytests():
+    x = 2
+    try:
+         x +=3 
+    except Exception as e: 
+        print("poop")
+    print(x)
+
+if __name__ == "__main__":
+    print(timetest)
     
+def filereader():
+    from pathlib import Path
+    testpath = "funproject/"
+    folder_loc = Path("C:/Users/delan/Documents/codebases/" + testpath + "testdoc.txt")
+    f = open(folder_loc)
+    print(f.read())
 
+def functest():
+    x= 1 
+    y = '1'
+    if x:
+        print('xyeet')
+    if y:
+        print('yyeet')
 
-    
-
+def minustest():
+    name = 'delaney'
+    newname = name[:-1] 
+    print(name + ' ' + newname)
 
