f65b5de1c828e9ed09f42f2c1e52bf8c63f45ef9
delaneymizell <83244330+delaneymizell@users.noreply.github.com>
2023-02-19 00:01:37 -0500

    finally finished black jack game

--- mainsection/fullgames/blackjack.py
+++ mainsection/fullgames/blackjack.py
@@ -1,28 +1,16 @@
+#this is an incredibly crude rendition of blackjack
+#the goal of this game was to pactise lists and functions
+#i learned alot about how to pass arguments through functions
+#but most imporntly that python is a pass by argument language so it was difficult
+#if any poor soul actuualy reads then and has any optimizations please let me know
 import random
 import os
-#game = True
-#while game == True: 
-def deck():
-    fulldeck = [2,3,4,5,6,7,8,9,10,"j","q","k","a"] * 4
-    import random
-    pickedcard = fulldeck.pop(random.randrange(0,len(fulldeck)))
-    print("this is the picked card", pickedcard)
-    return pickedcard
 
-def hit():
-    playerhand().append(deck)
-    
-
-    return randcard
-
-def dealerhand():
-    dhand = []
-    return dhand
-
-def playerhand():
-    phand = []
-    return phand
-
+def hit(person,deck):
+    pickedcard = deck.pop(random.randrange(0,len(deck)))
+    person.append(pickedcard)
+    return person
+        
 def sumhand(x):
     sum = 0
     for i in x:
@@ -38,25 +26,52 @@ def sumhand(x):
                     sum += 1
     return sum
 
-def dealerchoice():
-    dealerhand()
+def fullgame():
+    fulldeck = [2,3,4,5,6,7,8,9,10,"j","q","k","a"] * 4
+    playerhand = []
+    dealerhand = []
+    print("----------lets begin the game----------")
+    hit(playerhand,fulldeck),hit(playerhand,fulldeck),hit(dealerhand,fulldeck)
+    print("your first two cards are ", playerhand," for a total of ", sumhand(playerhand))
+    print("the dealers face up card is ", dealerhand," for a total of ", sumhand(dealerhand))
+    while True: 
+        if sumhand(playerhand) > 21:
+            print("you have busted loser ", playerhand)
+            gameover()
+        choice = input("would you like to hit? y/n")
+        if choice == "y":
+            hit(playerhand,fulldeck)
+            print("Your hand is now ", playerhand, " for a total of ", sumhand(playerhand))
+        else:
+            break
+    print("the dealers second card is ", hit(dealerhand,fulldeck), " for a total of ", sumhand(dealerhand))
+    while True:
+        if sumhand(dealerhand) < 17:
+            print("the dealer hit ", hit(dealerhand,fulldeck), " for a total of ", sumhand(dealerhand))
+        if sumhand(dealerhand) > 21:
+            print("the dealer has busted you win")
+            gameover()
+        else:
+            break
+    if sumhand(playerhand) > sumhand(dealerhand):
+        print("You have won congradulations")
+        gameover()
+    elif sumhand(playerhand) < sumhand(dealerhand):
+        print("you lost you big loser")
+        gameover()
+    elif sumhand(playerhand) == sumhand(dealerhand):
+        print("it is a tie and we all go home happy")
+        gameover()
+    else:
+        print("bad programming")
+        gameover()
     
+def gameover():
+    x = input("game is over, would you like to play again? y/n")
+    if x == "y" or "Y":
+        fullgame()
+    else:
+        print("??????????")
 
-
-
-
-
-randcard = deck[random.randrange(0,len(deck))]
-deck.remove(randcard)
-dealer = []
-dealertotal = []
-player = []
-playertotal = []
-
-
-dealer.append(randcard)
-randcard = deck[random.randrange(0,len(deck))]
-deck.remove(randcard)
-dealer.append(randcard)
-dealertotal = sum(dealer)
-print("dealers hand is ", dealertotal ,dealer)
+if __name__ == "__main__":
+    fullgame()
--- mainsection/practise/whitebaord.py
+++ mainsection/practise/whitebaord.py
@@ -1,20 +1,11 @@
-def func1():
-    shitlist = [1,2,"shit"]
-    return shitlist
+x = []
+while True:
+    choice = input()
+    if choice == "y":
+        x.append(1)
+    else:
+        break
+        print("print test 2")
+print("test1")
 
-def func2():
-    import random
-    addnum = random.randrange(0,10)
-    return addnum
-
-def func3(func1):
-    func1.append(func2())
-    print("this is append" ,func1)
-    return func1
-
-def func4():
-    print("before", func1())
-    func3(func1())
-    print("after", func1())
-if __name__ == "__main__":
-    func4()()
+print(x)
